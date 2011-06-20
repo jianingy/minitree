02e67f1eb2bc65ff432b647d7a2455abc2a6a48c
Jianing Yang <jianingy.yang@gmail.com>
2011-06-20 14:45:45 +0800

    added configuration files

--- main.py
+++ main.py
@@ -7,7 +7,29 @@ import logging
 import sys
 
 
-def main():
+def configure(ini_file):
+    from ConfigParser import SafeConfigParser as ConfigParser
+    import codecs
+    from StringIO import StringIO
+
+    default = """
+[server:main]
+port = 8000
+
+[backend:main]
+dsn = host=%(server)s port=%(port)s dbname=%(database)s \
+user=%(user)s password=%(password)s
+user =
+password =
+"""
+    p = ConfigParser()
+    p.readfp(StringIO(default))
+    with codecs.open(ini_file, "r", encoding="utf-8") as f:
+        p.readfp(f)
+    return p
+
+
+def main(c):
     from twisted.python import log
     from twisted.web import server
 
@@ -15,14 +37,14 @@ def main():
     observer.start()
     logging.basicConfig(file=sys.stderr, level=logging.DEBUG)
 
-    dbBackend.connect('host=localhost dbname=jianingy user=jianingy')
+    dbBackend.connect(c.get("backend:main", "dsn"))
 
     from minitree.service import root as site_root
     site = server.Site(site_root)
 
     from twisted.internet import reactor
-    reactor.listenTCP(8000, site)
+    reactor.listenTCP(int(c.get("server:main", "port")), site)
     reactor.run()
 
 if __name__ == '__main__':
-    main()
+    main(configure(sys.argv[1]))
