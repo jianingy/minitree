2d52594d4e868b2c768a5cfe8be2979e8d98e81e
Jianing Yang <jianingy.yang@gmail.com>
2011-06-24 10:43:43 +0800

    added .pyo to ignore

--- runtest.sh
+++ runtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export MINITREE_SERVER="http://127.0.0.1:8000"
-export MINITREE_DSN="host=localhost port=5432 user=postgres dbname=minitree"
+export MINITREE_DSN="host=localhost port=5432 user=jianingy dbname=jianingy"
 
 cd $(dirname $0)
 python test/select.py
